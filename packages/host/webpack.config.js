ec0cb2216f325143e1999e2daef3c3cabc379217
artyom-bashkatov <artyom.bashkatov@distillery.com>
2021-09-29 11:10:58 +0300

    Initial commit. Basic setup structure for micro-frontend apps

---
+++ packages/host/webpack.config.js
@@ -0,0 +1,51 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { ModuleFederationPlugin } = require("webpack").container;
+const ExternalTemplateRemotesPlugin = require("external-remotes-plugin");
+const path = require("path");
+const LiveReloadPlugin = require("webpack-livereload-plugin");
+
+module.exports = {
+  entry: "./src/index",
+  mode: "development",
+  devtool: "source-map",
+  optimization: {
+    minimize: false,
+  },
+  devServer: {
+    hot: true,
+    static: path.join(__dirname, "dist"),
+    port: 3000,
+    historyApiFallback: {
+      index: "index.html",
+    },
+  },
+  output: {
+    publicPath: "auto",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: ["@babel/preset-react"],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "host",
+      shared: { react: { singleton: true, eager: true }, "react-dom": { singleton: true, eager: true } },
+    }),
+    new ExternalTemplateRemotesPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new LiveReloadPlugin({
+      port: 35729,
+    }),
+  ],
+};
